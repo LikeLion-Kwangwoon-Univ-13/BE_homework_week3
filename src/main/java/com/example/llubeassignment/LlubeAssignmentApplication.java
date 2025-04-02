516377e92e5d508cb86c5b77d30f71f38d5ce091
leehyowon14 <leehyowon14@gmail.com>
2025-04-03 07:53:20 +0900

    feat: initional commit!

---
+++ src/main/java/com/example/llubeassignment/LlubeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.llubeassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlubeAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlubeAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/llubeassignment/LlubeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.llubeassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlubeAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
